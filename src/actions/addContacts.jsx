e538c76548ef90407acffcca8306d7c9e45893b7
keerthana1096 <rkkeerthana99@gmail.com>
2021-04-12 13:03:39 +0530

    indentation changes

--- src/actions/addContacts.jsx
+++ src/actions/addContacts.jsx
@@ -1,19 +1,19 @@
 import axios from 'axios';
 import CONFIG from './config';
-export default function addContacts(data,id){
+export default function addContacts(data, id) {
     var dataR;
-    axios.get(CONFIG.URL+"/Users/?="+id).then((response)=>{
-       dataR=response.data
+    axios.get(CONFIG.URL + "/Users/?=" + id).then((response) => {
+        dataR = response.data
     })
-    axios.patch(CONFIG.URL+"/Users/"+id+"/",{
-        contacts:[...dataR.contacts,
+    axios.patch(CONFIG.URL + "/Users/" + id + "/", {
+        contacts: [...dataR.contacts,
         {
 
-            name:data.name,
-            email:data.email,
-            PhoneNumber:data.PhoneNumber,
-            Company:data.Company,
-            Address:data.Address
+            name: data.name,
+            email: data.email,
+            PhoneNumber: data.PhoneNumber,
+            Company: data.Company,
+            Address: data.Address
         }]
     })
 }
--- src/actions/updateMessages.jsx
+++ src/actions/updateMessages.jsx
@@ -1,24 +1,24 @@
 import axios from 'axios';
 import CONFIG from './config'
-function patchMessages(userName,sentBy,message){
-    var res="";
+function patchMessages(userName, sentBy, message) {
+    var res = "";
 
-    axios.get(CONFIG.GET_USERS+"/?Username="+userName).then((response)=>{
-     res=response.data;
-     console.log(res);
-     axios.patch(CONFIG.URL+"/Users/"+res[0].id+"/",{
-       
-        messages:[
-            ...res[0].messages,{
-                sentBy:sentBy,
-                message:message
-            }
-        ]
-}).then((response)=>{
-console.log("update response",response)
-})
+    axios.get(CONFIG.GET_USERS + "/?Username=" + userName).then((response) => {
+        res = response.data;
+        console.log(res);
+        axios.patch(CONFIG.URL + "/Users/" + res[0].id + "/", {
+
+            messages: [
+                ...res[0].messages, {
+                    sentBy: sentBy,
+                    message: message
+                }
+            ]
+        }).then((response) => {
+            console.log("update response", response)
+        })
     })
-   
-    
+
+
 }
 export default patchMessages;
